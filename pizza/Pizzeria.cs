2efa041cc95f09ca89b996d4874a004b9d7ce6d7
school <school@A45PC16>
2025-04-22 09:18:04 +0300

    new5

--- pizza/Pizzeria.cs
+++ pizza/Pizzeria.cs
@@ -2,11 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace pizza
 {
-    internal class Pizza
+    internal class Pizzeria
     {
+        List<Pizza> pizzas = new();
+
+        public void AddPizza()
+        {
+            var newItem = new Pizza();
+
+
+            Console.Write("Enter pizza name: ");
+            newItem.Name = Console.ReadLine();
+            Console.Write("Enter pizza category: ");
+            newItem.Category = Console.ReadLine();
+            Console.Write("Enter pizza price: ");
+            newItem.Price = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter pizza quantity: ");
+            newItem.Quantity = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter pizza manufacture: ");
+            newItem.Size = Console.ReadLine();
+
+            pizzas.Add(newItem);
+        }
+        public void RemovePizza()
+        {
+            string jsonToSave = JsonSerializer.Serialize(pizzas);
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/pizza_db.json", jsonToSave);
+
+        }
+        public void LoadPizza()
+        {
+            string jsonToLoad = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/pizza_db.json");
+            pizzas = JsonSerializer.Deserialize<List<Pizza>>(jsonToLoad);
+        }
+        public void SavePiza()
+        {
+            foreach (var item in pizzas)
+            {
+                Console.WriteLine("------- Pizza ---------");
+                Console.WriteLine($"Name: {item.Name}");
+                Console.WriteLine($"Category: {item.Category}");
+                Console.WriteLine($"Price: {item.Price}");
+                Console.WriteLine($"Quantity: {item.Quantity}");
+                Console.WriteLine($"Size: {item.Size}");
+            }
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < pizzas.Count; ++i)
+                Console.WriteLine($"[{i}] Service: " + pizzas[i].Name);
+        }
+
+        public void Find()
+        {
+            Console.WriteLine("Enter number to delate:");
+            int numToDelete = Convert.ToInt32(Console.ReadLine());
+
+            if (numToDelete < 0 || numToDelete >= pizzas.Count)
+            {
+                Console.WriteLine("Number out of range!");
+                return;
+            }
+            pizzas.RemoveAt(numToDelete);
+            Console.WriteLine("Service deleted successfully!");
+        }
     }
 }
--- pizza/Program.cs
+++ pizza/Program.cs
@@ -29,7 +29,7 @@ Console.WriteLine("\t7.Pizza order by customer(s)");
 Console.WriteLine("\t8. Client calculation");
 
 
-List<Pizza> pizzas = new();
+
 
 
 while (true)
@@ -40,57 +40,21 @@ while (true)
     switch (choice)
     {
         case 1:
-            var newItem = new Pizza();
-
-
-            Console.Write("Enter pizza name: ");
-            newItem.Name = Console.ReadLine();
-            Console.Write("Enter pizza category: ");
-            newItem.Category = Console.ReadLine();
-            Console.Write("Enter pizza price: ");
-            newItem.Price = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter pizza quantity: ");
-            newItem.Quantity = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter pizza manufacture: ");
-            newItem.Size = Console.ReadLine();
-
-            pizzas.Add(newItem);       
+            
             break;
      
         case 2:
-            string jsonToSave = JsonSerializer.Serialize(pizzas);
-            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-            File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/pizza_db.json", jsonToSave);
+
             break;
         case 3:
-            string jsonToLoad = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/products_db.json");
-            pizzas = JsonSerializer.Deserialize<List<Pizza>>(jsonToLoad);
+            
             break;
         case 4:
-            foreach (var item in pizzas)
-            {
-                Console.WriteLine("------- Pizza ---------");
-                Console.WriteLine($"Name: {item.Name}");
-                Console.WriteLine($"Category: {item.Category}");
-                Console.WriteLine($"Price: {item.Price}");
-                Console.WriteLine($"Quantity: {item.Quantity}");
-                Console.WriteLine($"Size: {item.Size}");
-            }
+            
                 break;
         case 5:
-            for (int i = 0; i < pizzas.Count; ++i)
-                Console.WriteLine($"[{i}] Service: " + pizzas[i].Name);
-
-            Console.WriteLine("Enter number to delate:");
-            int numToDelete = Convert.ToInt32(Console.ReadLine());
-
-            if (numToDelete < 0 || numToDelete >= pizzas.Count)
-            {
-                Console.WriteLine("Number out of range!");
-                break;
-            }
-            pizzas.RemoveAt(numToDelete);
-            Console.WriteLine("Service deleted successfully!");
+           
+            
             break;
 
     }
